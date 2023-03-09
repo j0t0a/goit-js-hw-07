3f2561b8261f3b581254aa9b2e2804f88e78c4d6
Julia Aseeva <juliata0506@gmail.com>
2023-03-09 19:05:34 +0200

    second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+const imageElements = galleryItems.map( item => {
+    const galleryItem = document.createElement('a')
+    galleryItem.classList.add('gallery__item')
+    galleryItem.href = (item.original)
+
+    const galleryImage = document.createElement('img')
+    galleryImage.classList.add('gallery__image')
+    galleryImage.src = item.preview
+    galleryImage.dataset.source = item.original
+    galleryImage.alt = item.description
+
+    galleryItem.append(galleryImage)
+
+    return galleryItem
+})
+
+gallery.append(...imageElements)
+
+new SimpleLightbox('.gallery a', {
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
+
+
